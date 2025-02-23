e5cad9126c3d4e5bc2bd25e95bb3b322a14dbf1d
ethahd <ahd@unc.edu>
2025-02-23 00:04:41 -0500

    project creating commit

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ethan AK on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather/WeatherTests/WeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Ethan AK on 2/23/25.
+//
+
+import Testing
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
